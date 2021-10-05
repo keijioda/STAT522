e9ce45c288242c3c03610e16f2fabe5e68af7a9d
Keiji Oda <keiji.oda@gmail.com>
2021-10-05 13:26:31 -0700

    Lec 4 Python code done

--- Python/lecture04.py
+++ Python/lecture04.py
@@ -96,37 +96,9 @@ model3_fit = model3.fit()
 from statsmodels.stats.anova import anova_lm
 anova_lm(model3_fit, model2)
 
-# Visualize regressin surface
-# Create x1, x2 values to predict y for
-# x1 <- seq(.5, 1.5, by = .1)
-# x2 <- seq(10, 30, by = 1)
-
-# Predict y
-# y <- outer(x1, x2, function(x1, x2) {
-#   predict(model3, newdata = data.frame(ChgRate = x1, Celsius = x2))
-# })
-
-# Regression plane
-# persp(x1, x2, y, theta = 30, phi = 20, expand = 0.9, shade = 0.1)
-
-# Using interactive 3D plot
-# newdat <- expand.grid(ChgRate = x1, Celsius = x2)
-# newdat$Cycles <- predict(model3, newdata = newdat)
-
-# install.packages("rgl")
-# library(rgl)
-# with(newdat, plot3d(ChgRate, Celsius, Cycles, col = "blue", size = 1, type = "s"))
-# with(newdat, surface3d(unique(ChgRate), unique(Celsius), Cycles,
-#                        alpha = 0.3, front = "line", back = "line"))
-
 # Birthweigth and mother's smoking data
 url = "https://raw.githubusercontent.com/keijioda/STAT522/main/data/birthsmokers.txt"
 baby = pd.read_table(url)
-
-# Recode smoke
-
-# baby <- baby %>% 
-#   mutate(Smoke = if_else(Smoke == "yes", 1, 0))
 
 # Frequency table on mother's smoking
 baby.Smoke.value_counts()
@@ -140,22 +112,27 @@ pd.options.display.max_columns = None
 out
 
 # plot
-# baby %>% 
-#   ggplot(aes(x = Gest, y = Wgt, color = factor(Smoke))) + 
-#   geom_point() + 
-#   geom_smooth(method = "lm", se = FALSE)
+sns.lmplot(x = "Gest", y = "Wgt", hue = "Smoke", data = baby)
+plt.show()
+plt.clf()
+
+# Recode smoke
+baby["Smoke2"] = np.where(baby["Smoke"] == "yes", 1, 0)
 
 # Regression
-# fit <- lm(Wgt ~ Gest + factor(Smoke), data = baby)
-# summary(fit)
+baby_fit = smf.ols("Wgt ~ Gest + Smoke2", data = baby).fit()
+baby_fit.summary()
+
+# Or alternatively,
+baby_fit = smf.ols("Wgt ~ Gest + C(Smoke)", data = baby).fit()
+baby_fit.summary()
 
 # Regression by smoking
-# Require broom package
-# install.packages("broom")
-# baby %>% 
-#   group_by(Smoke) %>% 
-#   group_map(~broom::tidy(lm(Wgt ~ Gest, data = .x)))
-  
+# Compare estimated beta coefficients
+nsmk, ysmk = baby.groupby("Smoke").apply(lambda x: smf.ols("Wgt ~ Gest", data = x).fit())
+nsmk.params
+ysmk.params
+
 # Depression data
 # Change varirable names to lower cases
 url = "https://raw.githubusercontent.com/keijioda/Daniel/main/chap11/EXA_C11_S02_03.csv"
@@ -166,36 +143,32 @@ depress
 # Frequency table on treatment method
 depress.method.value_counts()
 
-# Scatterplot using ggplot2
-# depress %>% 
-#   ggplot(aes(x = age, y = effect, color = method)) +
-#   geom_point() + 
-#   geom_smooth(method = "lm", se = FALSE)
+# Scatterplot
+sns.lmplot(x = "age", y = "effect", hue = "method", data = depress)
+plt.show()
+plt.clf()
 
 # Regression using dummy variables
 # Set reference to method C
-# depress <- depress %>% 
-#   mutate(method = factor(method),
-#          method = relevel(method, ref = "C"))
-
-# Run regression
-# model1 <- lm(effect ~ age + factor(method), data = depress)
-# summary(model1)
+model1 = smf.ols("effect ~ age + C(method, Treatment(reference = 'C'))", data = depress).fit()
+model1.summary()
 
 # plot regression result
-# pred <- predict(model1)
-# cbind(depress, pred) %>% 
-#   ggplot(aes(x = age, y = effect, color = method)) +
-#   geom_point() + 
-#   geom_line(aes(y = pred))
+depress["fitted"] = model1.fittedvalues
+fig, ax = plt.subplots()
+sns.scatterplot(x = "age", y = "effect", hue = "method", data = depress, ax = ax, legend = None)
+sns.lineplot(x = "age", y = "fitted", hue = "method", data = depress, ax = ax)
+plt.show()
+plt.clf()
 
 # include interaction
-# model2 <- lm(effect ~ age * method, data = depress)
-# summary(model2)
+model2 = smf.ols("effect ~ age * C(method, Treatment(reference = 'C'))", data = depress).fit()
+model2.summary()
 
 # plot regression result
-# pred <- predict(model2)
-# cbind(depress, pred) %>% 
-#   ggplot(aes(x = age, y = effect, color = method)) +
-#   geom_point() + 
-#   geom_line(aes(y = pred))
+depress["fitted2"] = model2.fittedvalues
+fig, ax = plt.subplots()
+sns.scatterplot(x = "age", y = "effect", hue = "method", data = depress, ax = ax, legend = None)
+sns.lineplot(x = "age", y = "fitted2", hue = "method", data = depress, ax = ax)
+plt.show()
+plt.clf()
